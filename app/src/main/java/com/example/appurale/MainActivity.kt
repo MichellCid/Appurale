39bd89a30bfbfc69aacf08208b97d17f02ee2c44
justmariaam <mhdez914@gmail.com>
2026-03-14 20:18:07 -0600

    Pantalla de inicio

--- app/src/main/java/com/example/appurale/MainActivity.kt
+++ app/src/main/java/com/example/appurale/MainActivity.kt
@@ -3,15 +3,15 @@ package com.example.appurale
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.appurale.navegacion.*
+import com.example.appurale.navegacion.AppNavigation
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            NavegacionApp()
-
+            AppNavigation()
         }
     }
 }
